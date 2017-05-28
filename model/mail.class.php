9047833df4e0e9859c8c9c15936448e89c497068
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-28 12:54:46 +0200

    Removed some

--- model/mail.class.php
+++ model/mail.class.php
@@ -46,19 +46,4 @@
       }
     }
   }
-
-  $     // Add a recipient
-$mail->addAddress('ellen@example.com');               // Name is optional
-$mail->addReplyTo('info@example.com', 'Information');
-$mail->addCC('cc@example.com');
-$mail->addBCC('bcc@example.com');
-
-$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-
-
-
-
-
 ?>
