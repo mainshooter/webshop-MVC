d3faef89bde7f2e9d14fb614112b9c27ec06649e
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-29 10:43:39 +0200

    Added . to ,

--- model/shoppingcardview.php
+++ model/shoppingcardview.php
@@ -7,13 +7,13 @@
         $view .= '
           <div class="col-2">&nbsp;</div>
           <div class="col-8 product winkelmandje-height-center">
-            <img class="col-1" src="' . $key['pad'] . $key['filenaam'] . '">
-            <h2 class="col-5 left-text"><a href="?op=details&productID=' . $key['idProduct'] . '">' . $key['naam'] . '</a></h2>
-            <p class="col-1 left-text">&euro;' . str_replace('.', ',', $key['prijs']) . '</p>
-            <p class="col-1">Aantal: </p>';
+            <img src="' . $key['pad'] . $key['filenaam'] . '">
+            <h2 class="left-text"><a href="?op=details&productID=' . $key['idProduct'] . '">' . $key['naam'] . '</a></h2>
+            <p class="left-text">&euro;' . str_replace('.', ',', $key['prijs']) . '</p>
+            <p>Aantal: </p>';
             $view .= $this->generateOptionNumbers($key['idProduct'], $amount);
-            $view .= '<i class="fa fa-trash-o col-1" aria-hidden="true" style="margin-top: 0.5em;" onclick="shoppingcard.remove(' . $key['idProduct'] . ')"></i>
-            <p class="col-2">Totaal: &euro;' . $productTotal . '</p>
+            $view .= '<i class="fa fa-trash-o" aria-hidden="true" style="margin-top: 0.5em;" onclick="shoppingcard.remove(' . $key['idProduct'] . ')"></i>
+            <p>Totaal: &euro;' .str_replace('.', ',', $productTotal) . '</p>
           </div>
           <div class="col-2">&nbsp;</div>
           <div class="col-12"></div>
