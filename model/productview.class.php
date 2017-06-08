50b245461eb8e2a461f40d5529e627fdd6e50acf
marksalet <marksalet@gmail.com>
2017-06-08 17:09:32 +0200

    Added contact and some small changes

--- model/productview.class.php
+++ model/productview.class.php
@@ -10,7 +10,9 @@
             <div class="col-12 col-m-12">
               <img class="product_img" src="' . $key['pad'] . $key['filenaam'] . '" />
             </div>
-            <h2>' . $key['naam'] . '</h2>
+            <div class="col-12 product-title">
+              <h2>' . $key['naam'] . '</h2>
+            </div>
           </a>
           <p>&euro;' . str_replace('.', ',', $key['prijs']) . '</p>
           <i class="fa fa-cart-arrow-down" aria-hidden="true" onclick="shoppingcard.add(' . $key['idProduct'] . ');shoppingcard.goTo();"></i>
@@ -63,6 +65,7 @@
       $detail = '';
       foreach ($result as $key) {
         $detail .= '
+          <div class="col-1"></div>
           <div class="col-10 product_details">
             <h2 class="col-12">' . $key['naam'] . '</h2>
             <img class="col-3" src="' . $key['pad'] . $key['filenaam'] . '" />
@@ -70,6 +73,7 @@
             <p class="col-1">&euro;' . str_replace('.', ',', $key['prijs']) . '</p>
             <i class="fa fa-cart-plus col-5" aria-hidden="true" onclick="shoppingcard.add(' . $key['idProduct'] . ')"></i>
           </div>
+          <div class="col-1"></div>
         ';
     }
     return($detail);
--- model/shoppingcardview.class.php
+++ model/shoppingcardview.class.php
@@ -9,11 +9,11 @@
           <div class="col-8 col-m-12 product winkelmandje-height-center">
               <div class="col-1 col-m-1 webshop-img"><img class="col-12 webshop-img" src="' . $key['pad'] . $key['filenaam'] . '"></div>
               <div class="col-5 col-m-5"><h2 class="left-text"><a href="?op=details&productID=' . $key['idProduct'] . '">' . $key['naam'] . '</a></h2></div>
-              <div class="col-1 col-m-1">&euro;' . str_replace('.', ',', $key['prijs']) . '</div>
-              <div class="col-1 col-m-1 right-text">Aantal: </div>
-              <div class="col-1 col-m-1">'; $view .= $this->generateOptionNumbers($key['idProduct'], $amount); $view .='</div>
-              <div class="col-1 col-m-1"><i class="fa fa-trash-o" aria-hidden="true" style="margin-top: 0.5em;" onclick="shoppingcard.remove(' . $key['idProduct'] . ')"></i></div>
-              <div class="col-2 col-m-2">Totaal: &euro;' . $productTotal . '</div>
+              <div class="col-1 col-m-1" style="margin-top: 0.5em;">&euro;' . str_replace('.', ',', $key['prijs']) . '</div>
+              <div class="col-1 col-m-1 right-text" style="margin-top: 0.5em;">Aantal: </div>
+              <div class="col-1 col-m-2" style="margin-top: 0.5em;">'; $view .= $this->generateOptionNumbers($key['idProduct'], $amount); $view .='</div>
+              <div class="col-1 col-m-1" style="margin-top: 0.5em;"><i class="fa fa-trash-o" aria-hidden="true" onclick="shoppingcard.remove(' . $key['idProduct'] . ')"></i></div>
+              <div class="col-2 col-m-1" style="margin-top: 0.5em;">Totaal: &euro;' . $productTotal . '</div>
           </div>
           <div class="col-2 col-m-0">&nbsp;</div>
           <div class="col-12"></div>
@@ -26,7 +26,7 @@
       // Generates a select input field.
       // When the option is the same to the number we got for the option input
       // We set that as selected
-      $selectField = '<select onchange="shoppingcard.update(\'' . $productID . '\', this.value);" class="col-12">';
+      $selectField = '<select onchange="shoppingcard.update(\'' . $productID . '\', this.value);">';
       for ($i=0; $i < 10; $i++) {
         if ($amount == $i) {
           $selectField .= '<option value="' . $i .'" selected>' . $i . '</option>';
