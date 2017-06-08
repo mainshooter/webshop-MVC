790b79ed76d61d96e69ffa0e82cf4f9d3e45f94f
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-08 17:57:55 +0200

    View added for admin dashboard

--- view/admin/crud-dashboard.php
+++ view/admin/crud-dashboard.php
@@ -1,10 +1,24 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Product admin</title>
-  </head>
+<?php include 'header.html' ?>
   <body>
-    <a href="?op=productToevoegen">Product toevoegen</a>
+    <div class="row">
+      <a class="col-12" href="?op=productToevoegen">Product toevoegen</a>
+
+      <?php
+        echo "<table class='col-12'>";
+        foreach ($AllProducts as $key) {
+          echo '
+            <tr class="col-6">
+              <td class="col-4"><img class="col-12" src="' . $key['pad'] . $key['filenaam'] . '"/></td>
+              <td class="col-4"><h2>' . $key['naam'] . '</h2></td>
+              <td class="col-2">
+                <a><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                <i onclick="product.delete(' . $key['idProduct'] . ');" class="fa fa-trash-o" aria-hidden="true"></i>
+              </td>
+            </tr>
+          ';
+        }
+      ?>
+
+    </div>
   </body>
 </html>
