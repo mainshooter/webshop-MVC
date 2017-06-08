256cad8737d1b6de9a36b0d1c592b5fd41ff652a
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-08 18:57:17 +0200

    added link to product update

--- view/admin/crud-dashboard.php
+++ view/admin/crud-dashboard.php
@@ -11,7 +11,7 @@
               <td class="col-4"><img class="col-12" src="' . $key['pad'] . $key['filenaam'] . '"/></td>
               <td class="col-4"><h2>' . $key['naam'] . '</h2></td>
               <td class="col-2">
-                <a><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                <a href="?op=updateProductForm&productID=' . $key['idProduct'] . '"><i class="fa fa-pencil" aria-hidden="true"></i></a>
                 <i onclick="product.delete(' . $key['idProduct'] . ');" class="fa fa-trash-o" aria-hidden="true"></i>
               </td>
             </tr>
