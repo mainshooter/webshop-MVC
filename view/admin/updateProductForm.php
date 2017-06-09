043a037b762dae85e0634f59dcb242d5c4fe321c
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-09 09:11:53 +0200

    Added some corrections to the form

--- view/admin/updateProductForm.php
+++ view/admin/updateProductForm.php
@@ -5,12 +5,13 @@
 
       foreach ($productDetails as $key) {
         echo '
-          <form method="post" class="col-12">
+          <form method="post" enctype="multipart/form-data" class="col-12">
+            <img class="col-3" src="/leerjaar2/webshop/' . $key['pad'] . $key['filenaam'] .  '">
             <div>Product naam</div>
             <input type="text" name="product_name" value="' . $key['naam'] . '">
 
             <div>Prijs</div>
-            <input type="number" name="product_price" value="' . $key['prijs'] . '">
+            <input type="number" step="0.01" name="product_price" value="' . $key['prijs'] . '">
 
             <div>Product beschrijving</div>
             <textarea name="product_description">' . $key['beschrijving'] . '</textarea>
