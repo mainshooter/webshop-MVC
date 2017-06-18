6bc9bddd2feb53e31916df9102449b454f1f3302
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-18 16:33:19 +0200

    Removed the quicksand font

--- view/header.php
+++ view/header.php
@@ -9,7 +9,7 @@
    <link rel="stylesheet" href="style/grid.css" type="text/css">
    <link rel="stylesheet" href="style/style.css" type="text/css">
 
-   <link href="https://fonts.googleapis.com/css?family=Quicksand:300" rel="stylesheet">
+   <!-- <link href="https://fonts.googleapis.com/css?family=Quicksand:300" rel="stylesheet"> -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link rel="apple-touch-icon" sizes="57x57" href="file/icon/apple-icon-57x57.png">
