c08f303ea32851ed7650187b8c7f1fcc7294907d
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-18 15:20:06 +0200

    Corrected the request for the pages

--- view/pagenering.php
+++ view/pagenering.php
@@ -5,7 +5,7 @@
   <div class="pagenering col-12">
     <ul>';
   for ($i=0; $i < $pages; $i++) {
-    $list .= '<li><a href="?pageNumer=' . $i . '">' . $p=$i + 1 . '</a></li>';
+    $list .= '<li><a href="?op=page&pageNumer=' . $i . '">' . $p=$i + 1 . '</a></li>';
   }
   $list .= '</ul></div>';
 
