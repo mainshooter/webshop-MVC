bafb3f907c5719fe03bcbce1efda25cc47c14e8c
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-19 13:02:20 +0200

    Fixed the wrong placement of something

--- view/shoppingcard.php
+++ view/shoppingcard.php
@@ -2,6 +2,7 @@
 
 $teller = 0;
 $tellerForTimesPlaced = 0;
+// To place 3 items next to each other
     foreach ($product_details as $row) {
       foreach ($row as $key) {
         if ($tellerForTimesPlaced == 0) {
@@ -28,8 +29,8 @@ $tellerForTimesPlaced = 0;
         }
       }
     }
-    echo "<div class='col-12'><h2>BTW: &euro;" . $BTWPrice . "</h2>";
     echo "<h2>Exclusief BTW: &euro;" . $priceWithoutBTW . "</h2>";
+    echo "<div class='col-12'><h2>BTW: &euro;" . $BTWPrice . "</h2>";
     echo "<h2>Totaal: &euro;" . $totalPrice . "</h2></div>";
     echo "<div class='col-12'></div><div class='col-12'><a href='?op=createOrder'><button id='order' type='button'>Bestellen!</button></a></div>";
 ?>
