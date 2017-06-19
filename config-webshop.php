aa8138cbd89c241c300a73d8a97d4fb69cd08d75
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-19 07:21:03 +0200

    Added to the ignore

--- config-webshop.php
+++ config-webshop.php
@@ -4,11 +4,11 @@
   define('siteLocation', 'https://dev.samebestserver.nl/leerjaar2/webshop-MVC/');
 
   // Database configration
-  define('databaseLocation', 'samebestserver.nl');
-  define('databasePort', '100');
+  define('databaseLocation', 'localhost');
+  define('databasePort', '3306');
 
-  define('databaseUsername', 'multi');
-  define('databasePassword', '1234567890');
+  define('databaseUsername', 'root');
+  define('databasePassword', '1234');
 
   define('databaseName', 'multiversum');
 
