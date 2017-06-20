861308508ce409504a967ecec9ef90141f09a112
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-20 10:59:16 +0200

    Removed useless method

--- model/productview.class.php
+++ model/productview.class.php
@@ -2,27 +2,6 @@
 
   class Productview {
 
-    public function createNewProductsView($result) {
-      $products = '';
-      $products .= '<div class="col-2 col-m-1"></div>';
-      foreach ($result as $key) {
-        $products .= '
-        <div class="col-4 col-m-4 product">
-          <a href="?op=details&productID=' . $key['idProduct'] . '">
-            <div class="col-12 col-m-12">
-              <img class="product_img" src="' . $key['pad'] . $key['filenaam'] . '" />
-            </div>
-            <h2>' . $key['naam'] . '</h2>
-          </a>
-          <p>&euro;' . str_replace('.', ',', $key['prijs']) . '</p>
-          <i class="fa fa-cart-arrow-down" aria-hidden="true" onclick="shoppingcard.add(' . $key['idProduct'] . ');shoppingcard.goTo();"></i>
-        </div>
-        ';
-      }
-      $products .= '<div class="col-2 col-m-1"></div>';
-      return($products);
-    }
-
     public function createProductDetails($result) {
       $detail = '';
       foreach ($result as $key) {
