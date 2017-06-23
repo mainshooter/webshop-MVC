abc93d0a70400f25cec5f638cf91b4d69ab3374a
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-23 13:56:45 +0200

    Added order and price to the top

--- view/details.php
+++ view/details.php
@@ -5,10 +5,12 @@
         <div class="col-1"></div>
         <div class="col-10 product_details">
           <h2 class="col-12">' . $key['naam'] . '</h2>
-          <img class="col-3" src="' . $key['pad'] . $key['filenaam'] . '" />
+          <div class="col-3">
+            <img class="col-12" src="' . $key['pad'] . $key['filenaam'] . '" />
+            <button type="button" class="col-12" onclick="shoppingcard.add(' . $key['idProduct'] . ')"><i class="fa fa-cart-plus" aria-hidden="true"></i> Bestel</button>
+            <p class="col-1">&euro;' . str_replace('.', ',', $key['prijs']) . '</p>
+          </div>
           <div class="col-9">' . html_entity_decode(html_entity_decode($key['beschrijving'])) . '</div>
-          <p class="col-1">&euro;' . str_replace('.', ',', $key['prijs']) . '</p>
-          <button type="button" onclick="shoppingcard.add(' . $key['idProduct'] . ')"><i class="fa fa-cart-plus" aria-hidden="true"></i> Bestel</button>
         </div>
         <div class="col-1"></div>
       ';
